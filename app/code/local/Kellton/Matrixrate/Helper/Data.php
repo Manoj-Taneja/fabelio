8fa4d9df4e15e66518eb4b6a7db730b0d3a17b1e
Kamal Kumar <kamal.kumar@kelltontech.com>
2016-07-25 18:41:50 +0530

    attribute creation and updation

--- app/code/local/Kellton/Matrixrate/Helper/Data.php
+++ app/code/local/Kellton/Matrixrate/Helper/Data.php
@@ -171,7 +171,62 @@ class Kellton_Matrixrate_Helper_Data extends Mage_Core_Helper_Abstract
     
     public function set_delivery_time($product_id, $days){
         $_product= Mage::getModel('catalog/product')->load($product_id);
+        $arg_value="";
         if($days<=5){
+            $arg_value="3-5 Hari";
+            
+        }else if($days>5 && $days<=10){
+            $arg_value="Di bawah 2 Minggu";
+        }else if($days>10 && $days<=15){
+            $arg_value="Di bawah 3 Minggu";
+        }else{
+            $arg_value="4-6 Minggu";
+        }
+        $arg_attribute="deliverytime";
+        $attRibuteId=0;
+        $attribute = Mage::getModel('eav/config')->getAttribute('catalog_product', 'deliverytime');
+        $flag=0;
+
+        foreach ( $attribute->getSource()->getAllOptions(true, true) as $option )
+        {
+
+            if($arg_value == $option['label'])
+            {
+
+                unset($attribute);
+                $flag=1;
+                $attRibuteId = $option['value'] ; 
+            }
+        }
+        if($flag==0){
+            $attribute_model        = Mage::getModel('eav/entity_attribute');
+            $attribute_options_model= Mage::getModel('eav/entity_attribute_source_table') ;
+
+            $attribute_code         = $attribute_model->getIdByCode('catalog_product', $arg_attribute);
+            $attribute              = $attribute_model->load($attribute_code);
+
+            $attribute_table        = $attribute_options_model->setAttribute($attribute);
+            $options                = $attribute_options_model->getAllOptions(false);
+
+            $value['option'] = array($arg_value,$arg_value);
+            $result = array('value' => $value);
+            $attribute->setData('option',$result);
+            $attribute->save();
+
+            $attribute = Mage::getModel('eav/config')->getAttribute('catalog_product', $arg_attribute);
+            foreach ( $attribute->getSource()->getAllOptions(true, true) as $option )
+            {
+                if($arg_value == $option['label'])
+                {
+                    unset($attribute);
+                    $attRibuteId = $option['value'] ; 
+                }
+            }
+
+        }
+        $_product->setDeliverytime($attRibuteId);
+        $_product->save();
+       /* if($days<=5){
             $_product->setDeliverytime("3-5 Hari");
             
         }else if($days>5 && $days<=10){
@@ -180,10 +235,10 @@ class Kellton_Matrixrate_Helper_Data extends Mage_Core_Helper_Abstract
             $_product->setDeliverytime("Di bawah 3 Minggu");
         }else{
             $_product->setDeliverytime("4-6 Minggu");
-        }
+        }/**/
         
        // $_product->setDeliverytimeBackorder($days." Days");
-        $_product->save();
+        
     }
 
 }
